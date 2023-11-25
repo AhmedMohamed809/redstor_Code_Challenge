6b20f26ba4b9bb2cbdbc575b3444b5c8a83be298
Ahmed Mohamed <105979851+AhmedMohamed809@users.noreply.github.com>
2023-11-25 14:03:57 +0000

    Add project files.

---
+++ redstor_Code_Challenge/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+ public class Checkout
+{
+    // this is fields of this checkout class 
+    public Dictionary<char, int> prices;
+    public Dictionary<char, Tuple<int, int>> specialPrices;
+    public Dictionary<char, int> itemCounts;
+
+}
+
+
+class Program
+{
+static void Main(string[] args)
+    {
+        Console.WriteLine("Hi ");
+    }
+}
