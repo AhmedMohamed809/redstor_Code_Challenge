8c744d7d9536a7a82d1aad4295cb46de2c5d8a1a
Ahmed Mohamed <105979851+AhmedMohamed809@users.noreply.github.com>
2023-11-25 16:47:30 +0000

    add the unit and price

--- redstor_Code_Challenge/Program.cs
+++ redstor_Code_Challenge/Program.cs
@@ -3,18 +3,26 @@
 
  public class Checkout
 {
-    // this is fields of this checkout class 
+    // this is fields of the checkout class 
     public Dictionary<char, int> prices;
     public Dictionary<char, Tuple<int, int>> specialPrices;
     public Dictionary<char, int> itemCounts;
+    
 
 }
 
 
 class Program
 {
-static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hi ");
+      Dictionary<char,int> prices = new Dictionary<char, int>
+      {
+          { 'A', 60 },
+          { 'B', 30 },
+          { 'C', 30 },
+          { 'D', 25 }
+      };
+        
     }
 }
