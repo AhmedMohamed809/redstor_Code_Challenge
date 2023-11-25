40ce3363e1a0ecc52c3d2ee6693dfad24b880b53
Ahmed Mohamed <105979851+AhmedMohamed809@users.noreply.github.com>
2023-11-25 18:41:51 +0000

    add the scan method

--- redstor_Code_Challenge/Program.cs
+++ redstor_Code_Challenge/Program.cs
@@ -7,8 +7,37 @@
     public Dictionary<char, int> prices;
     public Dictionary<char, Tuple<int, int>> specialPrices;
     public Dictionary<char, int> itemCounts;
-    
 
+    // Methods 
+    public Checkout(Dictionary<char,int> prices , Dictionary<char,Tuple<int,int>> specialPrices) 
+    {
+       this.prices = prices;
+       this.specialPrices = specialPrices;
+       this.itemCounts = new Dictionary<char, int>();
+    
+    }
+    
+    public void Scan(char item)
+    {
+        if(!prices.ContainsKey(item))
+        {
+            Console.WriteLine($"Item '{item}' not found in pricing rules. ");
+
+            return; // to stop the program 
+        }
+        else if(!itemCounts.ContainsKey(item))
+        {
+
+            itemCounts[item] = 1;
+         
+        
+        }else
+        {
+            itemCounts[item]++;
+        }
+       
+    }
+  
 }
 
 
@@ -29,5 +58,10 @@ class Program
           { 'A', Tuple.Create(3, 150) },
           { 'B', Tuple.Create(2, 45) }
         };
+
+        Checkout checkout=new Checkout(prices, specialPrices);
+
+        checkout.Scan('A');
+       
     }
 }
