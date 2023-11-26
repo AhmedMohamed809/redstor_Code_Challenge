bc21059863de0aedd9514863c0bcc66d73236428
Ahmed Mohamed <105979851+AhmedMohamed809@users.noreply.github.com>
2023-11-26 13:32:30 +0000

    add  test2 (TestTotalPriceWithSpecialPricing)

--- TestProject2/UnitTest1.cs
+++ TestProject2/UnitTest1.cs
@@ -29,5 +29,36 @@ namespace TestProject2
 
             Assert.AreEqual(145, totalPrice); // Adjust the expected value based on your pricing logic
         }
+
+        [TestMethod]
+        public void TestTotalPriceWithSpecialPricing()
+        {
+            Dictionary<char, int> prices = new Dictionary<char, int>
+        {
+            { 'A', 60 },
+            { 'B', 30 },
+            { 'C', 30 },
+            { 'D', 25 }
+        };
+
+
+            Dictionary<char, Tuple<int, int>> specialPrices = new Dictionary<char, Tuple<int, int>>
+        {
+            { 'A', Tuple.Create(3, 150) },
+            { 'B', Tuple.Create(2, 45) }
+        };
+
+
+            Checkout checkout = new Checkout(prices, specialPrices);
+
+            checkout.Scan('A');
+            checkout.Scan('B');
+            checkout.Scan('B');
+            
+
+            int totalPrice = checkout.Total();
+
+            Assert.AreEqual(105, totalPrice); // Adjust the expected value based on your pricing logic
+        }
     }
 }
